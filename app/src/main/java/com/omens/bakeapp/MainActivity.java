90e12dc11c12edbb016fa186f40082f03d3297e3
Dmytro Verkhovsky <36972220+Dovahkiin169@users.noreply.github.com>
2020-09-02 22:19:09 +0200

    basic prime numbers app

--- app/src/main/java/com/omens/bakeapp/MainActivity.java
+++ app/src/main/java/com/omens/bakeapp/MainActivity.java
@@ -3,15 +3,42 @@ package com.omens.bakeapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText editTextFirstNumber,editTextSecondNumber;
+    Button buttonCountPrimes,buttonSendToDB;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.e("NumOFPrimes",String.valueOf(PrimeCounter(0,25)));
+
+        editTextFirstNumber = findViewById(R.id.editTextFirstNumber);
+        editTextSecondNumber = findViewById(R.id.editTextSecondNumber);
+        buttonCountPrimes = findViewById(R.id.buttonCountPrimes);
+        buttonSendToDB = findViewById(R.id.buttonSendToDB);
+    }
+
+    public void countPrimes(View view) {
+        if(isEditTextEmpty(editTextFirstNumber) && isEditTextEmpty(editTextSecondNumber)) {
+            int firstNumber = Integer.parseInt(editTextFirstNumber.getText().toString());
+            int secondNumber = Integer.parseInt(editTextSecondNumber.getText().toString());
+            Toast toast = Toast.makeText(getApplicationContext(), "There is " + PrimeCounter(firstNumber, secondNumber) + " prime numbers between entered numbers", Toast.LENGTH_LONG);
+            toast.show();
+        }
+    }
+    public boolean isEditTextEmpty(EditText editText) {
+        if (TextUtils.isEmpty(editText.getText().toString())) {
+            editText.setError("You need to enter number");
+            return false;
+        }
+        return true;
     }
 
     public int PrimeCounter(int firstNumber,int secondNumber) {
