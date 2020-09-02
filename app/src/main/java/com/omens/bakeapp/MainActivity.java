1c88abebe4d47db7786ad04df22fd61588c3d95b
Dmytro Verkhovsky <36972220+Dovahkiin169@users.noreply.github.com>
2020-09-02 21:48:13 +0200

    basic Prime function

--- app/src/main/java/com/omens/bakeapp/MainActivity.java
+++ app/src/main/java/com/omens/bakeapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.omens.bakeapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.e("NumOFPrimes",String.valueOf(PrimeCounter(0,25)));
+    }
+
+    public int PrimeCounter(int firstNumber,int secondNumber) {
+        int numberOfPrimes =0;
+        while (firstNumber < secondNumber) {
+            boolean flag = false;
+
+            for (int i = 2; i <= firstNumber / 2; ++i) {// condition for non prime number
+                if (firstNumber % i == 0) {
+                    flag = true;
+                    break;
+                }
+            }
+
+            if (!flag && firstNumber != 0 && firstNumber != 1) {
+                numberOfPrimes++;
+                Log.e("Primes", firstNumber + " ");
+            }
+            ++firstNumber;
+        }
+        return numberOfPrimes;
     }
 }
